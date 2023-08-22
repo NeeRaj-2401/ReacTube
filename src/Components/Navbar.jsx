4500345b70ac7e8370b255b5eae6d896c2c40437
Animesh <69345507+n-ce@users.noreply.github.com>
2023-08-22 10:51:19 +0530

    Update Navbar.jsx
    Signed-off-by: Animesh <69345507+n-ce@users.noreply.github.com>

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import axios from "axios";
 import "../App.css";
 import VideoModal from "./VideoModal.jsx";
 import LoaderModal from "./LoaderModal";
@@ -18,7 +17,6 @@ const Navbar = ({baseUrl}) => {
 
   // funtion to fetch search suggestion on realtime user input
   function handleSearchQueryChange(event) {
-    try {
       setSearchQuery(event.target.value);
       if (event.target.value.trim() === "") {
         //if input/searching section is empty then set suggestions to empty too.
@@ -27,17 +25,16 @@ const Navbar = ({baseUrl}) => {
       }
 
       // Call API for Search Suggestions
-      axios
-        .get(
+      fetch(
           `${baseUrl}/suggestions?query=${event.target.value}`
         )
         .then((res) => {
           //console.log(res.data);
           setSearchSuggestions(res.data);
+        })
+        .catch((error) => {
+          console.log({error});
         });
-    } catch (error) {
-      console.log({ error });
-    }
   }
 
   // function to Fetch Actual search results
@@ -45,11 +42,9 @@ const Navbar = ({baseUrl}) => {
     event.preventDefault();
     setIsLoading(true);
 
-    try {
       // call API or perform search here
   
-      axios
-        .get(
+      fetch(
           `${baseUrl}/search?q=${searchQuery}&filter=videos`
         )
         .then((res) => {
@@ -58,10 +53,10 @@ const Navbar = ({baseUrl}) => {
           setSearchResults(res.data.items);
           setNextPage(res.data.nextpage);
           setIsLoading(false);
+        })
+        .catch((error) => {
+          console.log({ error })
         });
-    } catch (error) {
-      console.log({ error });
-    }
   }
 
 
